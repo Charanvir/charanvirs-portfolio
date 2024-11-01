d20509e8fe2cb9030041cf6cfbda8b729600e901
Charanvir <charanvir123@gmail.com>
2024-11-01 19:39:17 -0400

    Made header responsive, and added hamburger navigation

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import Navigation from "../Navigation";
+import { FaBars, FaSun, FaMoon } from "react-icons/fa";
 
 function Header(props) {
   const {
@@ -10,10 +11,12 @@ function Header(props) {
     setIsDarkMode,
   } = props;
 
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <div className="headerClass">
       <header
-        className="px-3 py-4"
+        className="px-3 py-2"
         style={{
           backgroundColor: isDarkMode ? "#1a2f2f" : "#f5f5dc",
           borderBottom: isDarkMode ? "4px solid #f5d491" : "4px solid #1a2f2f",
@@ -21,27 +24,42 @@ function Header(props) {
       >
         <div className="container">
           <div className="row align-items-center">
-            <div className="col-12 col-md-4 text-start pageTitle">
-              <h1 style={{ color: isDarkMode ? "#f5d491" : "#343d46" }}>
+            <div className="col-6 text-start pageTitle">
+              <h1
+                style={{
+                  fontSize: "1.8rem",
+                  color: isDarkMode ? "#f5d491" : "#343d46",
+                }}
+              >
                 Charanvir
               </h1>
             </div>
-            <div className="col-12 col-md-5 text-center">
-              <Navigation
-                sections={sections}
-                currentSection={currentSection}
-                setCurrentSection={setCurrentSection}
-              />
-            </div>
-            <div className="col-12 col-md-3 text-end">
+            <div className="col-6 text-end">
               <button
                 className="toggle-icon-button"
                 onClick={() => setIsDarkMode(!isDarkMode)}
               >
-                <i className={`bi ${isDarkMode ? "bi-sun" : "bi-moon"}`}></i>
+                {isDarkMode ? <FaSun /> : <FaMoon />}
+              </button>
+              <button
+                className="menu-icon-button"
+                onClick={() => setMenuOpen(!menuOpen)}
+              >
+                <FaBars />
               </button>
             </div>
           </div>
+          {menuOpen && (
+            <div className="row">
+              <div className="col-12 text-center">
+                <Navigation
+                  sections={sections}
+                  currentSection={currentSection}
+                  setCurrentSection={setCurrentSection}
+                />
+              </div>
+            </div>
+          )}
         </div>
       </header>
     </div>
